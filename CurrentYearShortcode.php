89ebdc3073a951131485b7337a5a56dd6a63ca46
Joel.Gr <joel@gratcyk.com>
2025-05-10 15:50:06 -0500

    Update CurrentYearShortcode.php

--- CurrentYearShortcode.php
+++ CurrentYearShortcode.php
@@ -2,14 +2,24 @@
 /*
 Plugin Name: Current Year Shortcode
 Description: Adds a shortcode [year] to display the current year.
-Version: 1.0
+Version: 1.1
 Author: Joel Gratcyk
 Author URI: https://joel.gr
 */
 
-// Function to return current year
+/**
+ * Function to return the current year.
+ *
+ * @return string The current year.
+ */
 function current_year_shortcode() {
+    // Get the current year
     $year = date('Y');
-    return $year;
+
+    // Apply a filter to allow modification of the year
+    $year = apply_filters('current_year_shortcode_year', $year);
+
+    // Escape the output for safety
+    return esc_html($year);
 }
 add_shortcode('year', 'current_year_shortcode');
